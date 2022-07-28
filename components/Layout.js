703b7d16b351ad6f8e728928176ed44c12a41807
Nataniel Santos <nathan.santos159@hotmail.com>
2022-07-28 19:49:29 -0300

    feat: Implements basic structure layout

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+import styles from"../styles/Layout.module.css";
+
+export default function Layout({ children }) {
+  return (
+    <div className={styles.main_container}>
+      <Header />
+      { children }
+      <Footer />
+    </div>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Open Weather ☀️☁️</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
