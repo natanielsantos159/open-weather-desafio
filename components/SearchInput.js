3bf5746e984ce18c9b4782d3593fb78cb0a87ab5
Nataniel Santos <nathan.santos159@hotmail.com>
2022-07-30 17:45:01 -0300

    style: SearchInput component styles

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -2,26 +2,31 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import PlacesAutocomplete from "react-places-autocomplete";
+import styles from "../styles/SearchInput.module.css";
 
 export default function SearchInput() {
   const [address, setAddress] = useState("");
   const { t } = useTranslation();
 
   return (
-    <PlacesAutocomplete
-      value={address}
-      onChange={setAddress}
-    >
+    <PlacesAutocomplete value={address} onChange={setAddress}>
       {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
         <div>
           <input
-            {...getInputProps({ placeholder: t("Digite o nome da cidade") })}
+            {...getInputProps({
+              placeholder: t("Digite o nome da cidade"),
+              className: `${styles.search_input} ${
+                suggestions.length || loading > 0 ? styles.expanded : ""
+              }`,
+            })}
           />
-          <div>
-            {loading ? <div>{t('Carregando...')}</div> : null}
+          <div className={styles.suggestions}>
+            {loading ? <div>{t("Carregando...")}</div> : null}
             {suggestions.map((suggestion) => (
               <div
-                {...getSuggestionItemProps(suggestion)}
+                {...getSuggestionItemProps(suggestion, {
+                  className: styles.suggestion_item,
+                })}
                 key={suggestion.placeId}
               >
                 <Link href={`weather/${suggestion.placeId}/today`}>
