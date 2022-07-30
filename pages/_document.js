b881f66f96dc0a29436f46566cc25cdfaab121b7
Nataniel Santos <nathan.santos159@hotmail.com>
2022-07-30 14:21:50 -0300

    style: Sets global styles, font family and color variables

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,9 @@ export default class MyDocument extends Document {
             strategy="beforeInteractive"
             src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&libraries=places`}
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
           <Main />
