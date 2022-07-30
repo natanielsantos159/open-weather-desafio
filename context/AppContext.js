9d14253d210296361c073c0999efda026d1f1ee2
Nataniel Santos <nathan.santos159@hotmail.com>
2022-07-30 20:47:52 -0300

    feat: Translation state in context

--- context/AppContext.js
+++ context/AppContext.js
@@ -4,9 +4,13 @@ export const AppContext = createContext({})
 
 export const AppProvider = ({ children }) => {
   const [degree, setDegree] = useState('celsius');
+  const [lang, setLang] = useState('pt-BR');
+
   const values = {
     degree,
     setDegree,
+    lang,
+    setLang
   }
   return (
     <AppContext.Provider value={values}>
