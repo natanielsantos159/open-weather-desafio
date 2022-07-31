bb2062ac0dfaf868a5d812855c500dd760b66314
Nataniel Santos <nathan.santos159@hotmail.com>
2022-07-31 12:21:44 -0300

    style: Footer  alignment

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,10 @@
 import React from "react";
 import LanguageSwitcher from "./LanguageSwitcher";
+import styles from '../styles/Footer.module.css';
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <LanguageSwitcher />
     </footer>
   );
