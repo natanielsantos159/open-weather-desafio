4a3207a4e1028f9a58cabb18e522fc2c32e89bc3
Nataniel Santos <nathan.santos159@hotmail.com>
2022-07-31 17:15:14 -0300

    style: FiveDays forecast details styling

--- pages/weather/[placeId]/5-day.js
+++ pages/weather/[placeId]/5-day.js
@@ -3,11 +3,14 @@ import { AppContext } from "../../../context/AppContext";
 import getGeocodeByPlaceId from "../../../services/geocode.service";
 import weatherApi from "../../../services/weather.api.service";
 import translations from "../../../public/locales";
+import styles from "../../../styles/FiveDays.module.css";
+import Image from "next/image";
 
 export const getStaticProps = async (context) => {
   const { placeId } = context.params;
   const { data } = await getGeocodeByPlaceId(placeId);
   const { lat, lng } = data.results[0].geometry.location;
+
   const wheatherInfo = await fetcheNextFiveDays({ lat, lng });
   return {
     props: {
@@ -37,12 +40,12 @@ const fetcheNextFiveDays = async ({ lat, lng }) => {
     return {
       temp: {
         celsius: {
-          min: item.main.temp_min,
-          max: item.main.temp_max,
+          min: Math.floor(item.main.temp_min),
+          max: Math.floor(item.main.temp_max),
         },
         fahrenheit: {
-          min: (item.main.temp_min * 9) / 5 + 32,
-          max: (item.main.temp_max * 9) / 5 + 32,
+          min: Math.floor((item.main.temp_min * 9) / 5 + 32),
+          max: Math.floor((item.main.temp_max * 9) / 5 + 32),
         },
       },
       date: item.dt_txt,
@@ -62,30 +65,46 @@ export default function FiveDays({ wheatherInfo }) {
 
   const capitalize = (word) => {
     return word[0].toUpperCase() + word.slice(1).toLowerCase();
-  }
+  };
 
   const getParsedDate = (dateString) => {
     const date = new Date(dateString);
-    const day = new Intl.DateTimeFormat(lang, {day: 'numeric'}).format(date);
-    const month = new Intl.DateTimeFormat(lang, {month: 'long'}).format(date);
-    const weekday = new Intl.DateTimeFormat(lang, {weekday: 'long'}).format(date);
+    const day = new Intl.DateTimeFormat(lang, { day: "numeric" }).format(date);
+    const month = new Intl.DateTimeFormat(lang, { month: "long" }).format(date);
+    const weekday = new Intl.DateTimeFormat(lang, { weekday: "long" }).format(
+      date
+    );
     return `${capitalize(weekday).slice(0, 3)}, ${day} ${capitalize(month).slice(0, 3)}`;
-  }
+  };
 
   return (
-    <div>
-      <div>{city}</div>
-      <div>
-        {weatherList &&
-          weatherList.map((info, i) => (
-            <div key={i}>
-              <div>{getParsedDate(info.date)}</div>
-              <div>{info.temp[degree].min}</div>
-              <div>{info.temp[degree].max}</div>
-              <div>{translations[lang].weathers[info.description.split(' ').join("_")] || ''}</div>
-            </div>
+    wheatherInfo && (
+      <div className={styles.forecast_info}>
+        <h1>{city.toUpperCase()}</h1>
+        <p>{translations[lang].titles.five_days_forecast}</p>
+
+        <table className={styles.day_details_table}>
+          {weatherList.map((info, i) => (
+            <tr className={styles.day_details_row} key={i}>
+              <td className={styles.weather_date}>{getParsedDate(info.date)}</td>
+              <Image
+                src={`http://openweathermap.org/img/wn/${info.icon}@2x.png`}
+                alt={`${info.description} icon`}
+                height="40px"
+                width="40px"
+              />
+              <td className={styles.temp_min}>{info.temp[degree].min}</td>
+              <td className={styles.temp_line}></td>
+              <td className={styles.temp_max}>{info.temp[degree].max}</td>
+              <td>
+                {translations[lang].weathers[
+                  info.description.split(" ").join("_")
+                ] || info.description}
+              </td>
+            </tr>
           ))}
+        </table>
       </div>
-    </div>
+    )
   );
 }
